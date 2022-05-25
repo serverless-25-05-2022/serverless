b69ca3d84cdc706e2798beb532c377ca3bc43121
mohammadsaad99 <mohammadsaad_99@hotmail.com>
2022-05-25 16:51:29 +0300

    first commit

--- GetRating.cs
+++ GetRating.cs
@@ -16,7 +16,7 @@ namespace Company.Function
             [CosmosDB("mhmd-ratings-db", "ratingscontainer", ConnectionStringSetting = "RatingsDatabase", SqlQuery = "Select * from ratings r where r.id = {id}")]IEnumerable<RatingModel> rating,
             ILogger log)
         {
-            log.LogInformation("Gettingss Ratingss");           
+            log.LogInformation("Gettingss Ratingsss");           
             if (rating == null)
             {   
                 return new NotFoundResult();
